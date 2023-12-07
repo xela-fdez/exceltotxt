6032a41f9a5adffa0d0779e3599f8266135984f3
Alex Fernandez <santialex.f@gmail.com>
2023-12-07 22:15:30 +0100

    first commit

---
+++ src/main/java/excel/exceltotxt/App.java
@@ -0,0 +1,13 @@
+package excel.exceltotxt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
